61f6273995a0f8a06097eb894a39fd5c6e44a88f
Anthony <hello@anthonytran.me>
2023-08-20 22:19:37 -0700

    completed: structs, methods & interfaces

--- perimeter/perimeter_test.go
+++ perimeter/perimeter_test.go
@@ -17,6 +17,25 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
+	areaTests := []struct{
+		name string
+		shape Shape
+		want float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.want)
+			}
+		})
+	}
+
 	checkArea := func (t testing.TB, shape Shape, want float64)  {
 		t.Helper()
 		got := shape.Area()
