bcb10b2826102039063185dd8ad88e0ebe59c7c1
Anthony Tran <hello@anthonytran.me>
2023-09-07 01:22:57 -0700

    feat: up to select

---
+++ pointers/pointers_and_errors.go
@@ -0,0 +1,31 @@
+package pointers
+
+import "fmt"
+
+type Bitcoin int 
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+var ErrInsufficientFunds = fmt.Errorf("cannot withdraw, insufficient funds")
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+	w.balance -= amount
+	return nil
+}
